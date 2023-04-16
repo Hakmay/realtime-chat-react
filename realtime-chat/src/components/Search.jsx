78bad8973b19f0d257b062b36a579ccab80e7c00
Hakmay <haakim76@hotmail.fr>
2023-04-16 18:26:28 +0200

    add search function and create firestore collections

--- realtime-chat/src/components/Search.jsx
+++ realtime-chat/src/components/Search.jsx
@@ -1,22 +1,90 @@
-import React from 'react'
-import AvatarImage from "../assets/avatarDefault.jpg"
+import React, { useContext, useState } from "react";
+import { db } from "../firebase";
+import { doc, collection, query, where, getDoc, getDocs, setDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
 
 function Search() {
+  const [username, setUserName] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const {currentUser} = useContext(AuthContext)
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      });
+
+    }catch(error) {
+      setError(true)
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = async () => {
+    //check whether the groupe exists, if not create new one
+    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+    try {
+      const response = await getDoc(doc(db, "chats", combinedId));
+      if (!response.exists()) {
+        // create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), {messages: []});
+
+        // create user chats
+        await updateDoc(doc(db, "usersChats", currentUser.uid), {
+          [combinedId+".userInfo"]: {
+            uid:user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId+".date"]: serverTimestamp()
+        });
+
+        await updateDoc(doc(db, "usersChats", user.uid), {
+          [combinedId+".userInfo"]: {
+            uid:currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId+".date"]: serverTimestamp()
+        });
+      }
+    
+    } catch (error) {}
+    setUser(null);
+    setUserName("")
+    
+  }
+
   return (
     <div className="search">
-       <div className="searchForm">
-            <input type="text" name="" id="" placeholder="Find a user"/>
-       </div>     
-       <div className="userChat">
-        <img src={AvatarImage} alt="" />
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="Find a user"
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKey}
+          value={username}
+        />
+      </div>
+      {error && <span> User not found</span>}
+      {user && <div className="userChat" onClick={handleSelect}>
+        <img src={user.photoURL} alt="" />
         <div className="userChatInfo">
-          <span>John</span>
-          <p>Hello</p>
+          <span>{user.displayName}</span>
         </div>
-       </div>
+      </div>}
     </div>
-
-  )
+  );
 }
 
-export default Search
+export default Search;
